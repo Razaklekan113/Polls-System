7a0afc2310682b9c37a7b741d0de067226aefed6
Razaklekan113 <razaklekan11@gmail.com>
2024-12-04 09:29:28 +0100

    first commit

---
+++ polls/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
